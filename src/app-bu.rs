5519d4c0233f455750206952f4925f16d67540ec
Alex Black <wa7@sa34.cx>
2021-07-07 11:21:59 +0300

    Wednesday

--- src/app-bu.rs
+++ src/app-bu.rs
@@ -1,8 +1,10 @@
 use crate::db::DB;
 use crate::db::error::Error;
 // use std::error::Error;
+use tokio::fs::File;
+use tokio::io;
 
-pub fn get_rand_line_from_db(filename: &str) -> Result<String, Error> {
+pub async fn get_rand_line_from_db(filename: &str) -> Result<String, Error> {
     let db = DB::establish(&filename)?;
     let line = db.get_rand_line()?;
     Ok(line)
--- src/db/error.rs
+++ src/db/error.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::error;
 
 #[derive(Debug)]
 pub enum Error {
@@ -13,10 +14,12 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self {
             Error::NoEnv => write!(f, "DB_PATH must be set in .env"),
-            Error::ErrConnection => write!(f, "Database file not found"),
-            Error::ErrReading => write!(f, "Error reading from database"),
-            Error::EmptyDB => write!(f, "Database is empty"),
-            Error::ErrRand => write!(f, "Error with rand"),
+            Error::ErrConnection => write!(f, "database file not found"),
+            Error::ErrReading => write!(f, "error reading from database"),
+            Error::EmptyDB => write!(f, "database is empty"),
+            Error::ErrRand => write!(f, "error with rand"),
         }
     }
 }
+
+impl error::Error for Error {}
